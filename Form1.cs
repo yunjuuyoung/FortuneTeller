13e7c5ba81c7ee12d7d6016f35222cca28349d85
juyoung07 <yunju5090@gmail.com>
2025-08-27 08:55:10 +0900

    #2 내역 창이 중복으로 뜨는 것을 처리

--- Form1.cs
+++ Form1.cs
@@ -44,8 +44,16 @@ namespace FortuneTeller
 
         private void 상담내역불러오기ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            FormHistory form = new FormHistory();
-            form.Show();
+            FormHistory form = Application.OpenForms["FormHistory"] as FormHistory;
+            if (form != null)
+            {
+                form.Activate();
+            } 
+            else
+            {
+                form = new FormHistory();
+                form.Show();
+            }
         }
 
         private void 포츈텔러정보ToolStripMenuItem_Click(object sender, EventArgs e)
